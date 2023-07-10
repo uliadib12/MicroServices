8232ee3e14559557aea5c84c8151835882743f64
uliadib12 <uliadib@gmail.com>
2023-07-10 20:56:03 +0700

    add mongo

--- user-service/index.js
+++ user-service/index.js
@@ -1,11 +1,46 @@
+const dotenv = require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const mongoose = require('mongoose');
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const URL_MONGO = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.qu1ssig.mongodb.net/userDB?retryWrites=true&w=majority`
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+async function connectToMongo() {
+  await mongoose.connect(URL_MONGO)
+                  .then(() => console.log('Connected to MongoDB'));
+}
+
+async function main() {
+  await connectToMongo();
+
+  const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+  });
+  
+  const User = mongoose.model('user', userSchema);
+
+  const user = new User({
+    name: 'John',
+    email: 'john@gmail.com',
+    password: '123456',
+  });
+
+  await user.save();
+  console.log('User created');
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+}
+
+main();
